f74e16ff660447b6b34f7ed09be824e2a2b4e8e2
MOHAMMEDJUMA573 <188134218+MOHAMMEDJUMA573@users.noreply.github.com>
2025-08-21 20:04:47 +0300

    added index url to apps

--- src/cbcsync/settings.py
+++ src/cbcsync/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #my apps for the project
+    'accounts',
+    'assessments',
+    'courses',
+    'materials',
+    'schools',
+    'comms',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +61,7 @@ ROOT_URLCONF = 'cbcsync.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- src/cbcsync/urls.py
+++ src/cbcsync/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from . import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index, name="Home"),
+    path('account/', include('accounts.urls')),
+    path('assessment/', include('assessments.urls')),
+    path('school/', include('schools.urls')),
+    path('course/', include('courses.urls')),
+    path('material/', include('materials.urls')),
 ]
