c3cb1f88d00a0f4e8c04e7e7a73064c58df98590
MOHAMMEDJUMA573 <188134218+MOHAMMEDJUMA573@users.noreply.github.com>
2025-08-30 11:25:00 +0300

    Changed index to dashboard

--- src/cats/urls.py
+++ src/cats/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'cats' 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="dashboard")
 ]
--- src/parents/urls.py
+++ src/parents/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'parents' 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="dashboard")
 ]
